f22be96c14ed9dbe3f2c1dfcc4be18a756c3e25d
Jutho <jutho.haegeman@ugent.be>
2023-03-12 23:38:48 +0100

    improve coverage2

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,11 +15,13 @@ include("blasmultests.jl")
 
 println("Running tests multi-threaded:")
 Strided.enable_threads()
+Strided.set_num_threads(Base.Threads.nthreads()+1)
 include("othertests.jl")
 include("blasmultests.jl")
 
 Strided.enable_threaded_mul()
 include("blasmultests.jl")
+Strided.disable_threaded_mul()
 
 using Aqua
 Aqua.test_all(Strided)
