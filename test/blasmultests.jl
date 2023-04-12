5d380a2a58d7d9600b442089a569fe4f384dd165
lkdvos <lukas.devos@ugent.be>
2023-04-12 19:01:26 +0200

    update tests

--- test/blasmultests.jl
+++ test/blasmultests.jl
@@ -30,5 +30,33 @@ for T1 in (Float32, Float64, Complex{Float32}, Complex{Float64})
                 end
             end
         end
+        
+        @testset "Tensor product with StridedView: $T1 times $T2" begin
+            d = 10
+            A1 = rand(T1, (d, 1))
+            A2 = rand(T2, (1, d))
+            T3 = promote_type(T1, T2)
+            A3 = rand(T3, (d, d))
+            A4 = rand(T3, (d, d))
+            B1 = StridedView(copy(A1))
+            B2 = StridedView(copy(A2))
+            B3 = StridedView(copy(A3))
+            B4 = StridedView(copy(A4))
+            α = randn(T3)
+            β = randn(T3)
+            for op1 in (identity, conj)
+                @test op1(A1) == op1(B1)
+                for op2 in (identity, conj)
+                    @test op1(A1) * op2(A2) ≈ op1(B1) * op2(B2)
+                    for op3 in (identity, conj, transpose, adjoint)
+                        α = randn(T3)
+                        β = randn(T3)
+                        copyto!(B3, B4)
+                        mul!(op3(B3), op1(B1), op2(B2), α, β)
+                        @test B3 ≈ op3(β) * A4 + op3(α * op1(A1) * op2(A2)) # op3 is its own inverse
+                    end
+                end
+            end
+        end
     end
 end
